a69b14c163910ebec0e0e01b5fe523bcf831188f
sujing.su <sujing.su@bytedance.com>
2023-07-14 16:25:35 +0800

    update

--- subordinates/Static-Equilibrium-Structures/p41.f90
+++ subordinates/Static-Equilibrium-Structures/p41.f90
@@ -6,18 +6,18 @@ SUBROUTINE p41(input_file,output_file)
     USE main
     USE geom
     IMPLICIT NONE
-    CHARACTER(len=60),INTENT(IN) :: input_file
-    CHARACTER(len=60),INTENT(OUT) :: output_file
-    INTEGER,PARAMETER::iwp=SELECTED_REAL_KIND(15)
+    CHARACTER(len=60),INTENT(IN) :: input_file ! 用于声明p41的入参，输入参数
+    CHARACTER(len=60),INTENT(OUT) :: output_file ! 用于声明p41的入参，输出参数
+    INTEGER,PARAMETER::iwp=SELECTED_REAL_KIND(15) ! 这是对iwp的声明，其类型是整数（integer），并且被定义为参数（parameter）。参数在程序执行期间是常量值，不能被修改
     INTEGER::fixed_freedoms,i,iel,k,loaded_nodes,ndof=2,nels,neq,nlen,nod=2, &
     nodof=1,nn,nprops=1,np_types,nr
     REAL(iwp)::penalty=1.0e20_iwp,zero=0.0_iwp
     ! CHARACTER(LEN=15)::argv
     CHARACTER(LEN=30)::file_p41
     !-----------------------dynamic arrays------------------------------------
-    INTEGER,ALLOCATABLE::etype(:),g(:),g_g(:,:),kdiag(:),nf(:,:),no(:),      &
+    INTEGER,ALLOCATABLE::etype(:),g(:),g_g(:,:),kdiag(:),nf(:,:),no(:),      & ! 声明整型数组
     node(:),num(:) 
-    REAL(iwp),ALLOCATABLE::action(:),eld(:),ell(:),km(:,:),kv(:),loads(:),   &
+    REAL(iwp),ALLOCATABLE::action(:),eld(:),ell(:),km(:,:),kv(:),loads(:),   & ! 声明实数数组
     prop(:,:),value(:)
     !-----------------------input and initialisation--------------------------
     !CALL getname(argv,nlen)
@@ -30,13 +30,13 @@ SUBROUTINE p41(input_file,output_file)
     READ(10,*)nels,np_types
     nn=nels+1
     ALLOCATE(g(ndof),num(nod),nf(nodof,nn),etype(nels),ell(nels),eld(ndof),  &
-    km(ndof,ndof),action(ndof),g_g(ndof,nels),prop(nprops,np_types))
+    km(ndof,ndof),action(ndof),g_g(ndof,nels),prop(nprops,np_types)) ! 动态分配数组内存
     READ(10,*)prop
-    etype=1
+    etype=1 ! 单元类型
     IF(np_types>1)READ(10,*)etype
-    READ(10,*)ell
-    nf=1
-    READ(10,*)nr,(k,nf(:,k),i=1,nr)
+    READ(10,*)ell ! 单元长度数组
+    nf=1 ! 将整个数组的所有元素都设置为1
+    READ(10,*)nr,(k,nf(:,k),i=1,nr) ! 重复读取nr行数据，并按行依次存入二维数组nf中
     CALL formnf(nf)
     neq=MAXVAL(nf) 
     ALLOCATE(kdiag(neq),loads(0:neq))
